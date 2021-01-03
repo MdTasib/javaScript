ec34d565af714e8d2d95ced45ed59486c241a0b3
ohidul alam <mdtasib442@gmali.com>
2021-01-03 23:29:46 +0600

    javaScript modules 78 done

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+// how to use find and findIndex function
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// var result = arr.find(function (value) {
+// 	return value === 5 // return value === 44 --> undefind
+// })
+// console.log(result)
+
+// var result = arr.findIndex(function (index) {
+// 	return index === 5 // only index number print
+// })
+// console.log(result)
+
+// how to implement find and findIndex function
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// function myFind(arr, cb) {
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (cb(arr[i])) {
+// 			return arr[i] // only value print
+// 			// return i // only index number print
+// 		}
+// 	}
+// }
+
+// var result = myFind(arr, function (value) {
+// 	// return value === 55 // undefind
+// 	return value === 5
+// })
+// console.log(result)
