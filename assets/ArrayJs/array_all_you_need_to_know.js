03101371ec71bd862bea669ec708b2ef09553754
ohidul alam <mdtasib442@gmali.com>
2021-02-25 22:40:44 +0600

    a

---
+++ assets/ArrayJs/array_all_you_need_to_know.js
@@ -0,0 +1,8 @@
+// Array Constructor vs Factory
+// Constructor Patten
+const arr1 = new Array(1, 2, 3);
+console.log('Constructor : ', arr1, 'length : ', arr1.length);
+
+// Factory Patten
+const arr2 = Array(1, 2, 3);
+console.log('Factory : ', arr2, 'langth : ', arr2.length);
