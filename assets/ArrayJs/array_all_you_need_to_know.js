6c6a1b15f961e4e89a8f0b8cd594327d93a63c24
ohidul alam <mdtasib442@gmali.com>
2021-02-26 00:35:13 +0600

    pracitce

--- assets/ArrayJs/array_all_you_need_to_know.js
+++ assets/ArrayJs/array_all_you_need_to_know.js
@@ -1,3 +1,4 @@
+/*
 // Array Constructor vs Factory
 // Constructor Patten
 const arr1 = new Array(1, 2, 3);
@@ -6,3 +7,35 @@ console.log('Constructor : ', arr1, 'length : ', arr1.length);
 // Factory Patten
 const arr2 = Array(1, 2, 3);
 console.log('Factory : ', arr2, 'langth : ', arr2.length);
+*/
+
+
+/*
+// Traverse Array Elements
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// simple array traverse
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// array sum
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+}
+console.log(sum);
+
+// large number
+const numbers = [1, 4, 2, 6, 8, 44, 23, 5, 73];
+
+let largeNumber = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largeNumber) {
+        largeNumber = numbers[i]
+    }
+}
+console.log(largeNumber);
+*/
