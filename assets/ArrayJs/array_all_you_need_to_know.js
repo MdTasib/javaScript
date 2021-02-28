214f9c977bdf700918641d6029538264d6ff5279
ohidul alam <mdtasib442@gmali.com>
2021-02-28 23:39:43 +0600

    s

--- assets/ArrayJs/array_all_you_need_to_know.js
+++ assets/ArrayJs/array_all_you_need_to_know.js
@@ -203,3 +203,9 @@ class Stack {
     }
 }
 */
+
+/*
+// For Each Method in Array
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.forEach((number, index) => console.log('index number : ', index, 'value : ', number));
+*/
