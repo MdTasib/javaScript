a8e46dfebb921770bbac743cf0202e9e34ed0115
OhidulAlam <mdtasib442@gmail.com>
2021-03-12 22:55:37 +0600

    practice

--- assets/ArrayJs/array_all_you_need_to_know.js
+++ assets/ArrayJs/array_all_you_need_to_know.js
@@ -453,4 +453,22 @@ bigArray.reduce((acc, cur) => {
 }, [])
 
 console.timeEnd('Reduce')
+
+
+// Javascript Reduce Function
+const arr = [1, 2, 3, 4, 5, 6];
+
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log('Sum = ' + sum);
+
+const result = arr.reduce((a, b) => {
+    return a + b;
+})
+console.log('result = ' + result);
+
+
+
 */
